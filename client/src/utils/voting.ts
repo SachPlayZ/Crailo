83fa3e794109a2f08d0959eb9f2592c0777ff61c
Roshankhan6786 <useranroid0@gmail.com>
2025-06-22 08:42:45 +0530

    utils more

---
+++ client/src/utils/voting.ts
@@ -0,0 +1,62 @@
+import { validatorABI, validatorAddress } from "@/app/abi";
+import { parseEther } from "viem";
+import { useReadContract, useWriteContract } from "wagmi";
+
+export const useVote = () => {
+    const { writeContractAsync, isPending } = useWriteContract();
+
+    const voteYes = async (proposalId: number) => {
+        try {
+            const tx = await writeContractAsync({
+                address: validatorAddress,
+                abi: validatorABI,
+                functionName: "voteYes",
+                args: [proposalId],
+            });
+
+            if (tx) {
+                console.log("Successful Transaction", tx);
+                return tx;
+            }
+        } catch (error) {
+            console.error("Transaction failed:", error);
+            throw error;
+        }
+    };
+    const voteNo = async (proposalId: number) => {
+        try {
+            const tx = await writeContractAsync({
+                address: validatorAddress,
+                abi: validatorABI,
+                functionName: "voteNo",
+                args: [proposalId],
+            });
+
+            if (tx) {
+                console.log("Successful Transaction", tx);
+                return tx;
+            }
+        } catch (error) {
+            console.error("Transaction failed:", error);
+            throw error;
+        }
+    };
+
+    return { voteYes, voteNo, isPending };
+};
+
+export const useStatus = () => {
+    const {
+        data: status,
+        refetch: refetchStatus,
+        isLoading: isStatusLoading,
+        isError,
+        error,
+    } = useReadContract({
+        address: validatorAddress,
+        abi: validatorABI,
+        functionName: "getProductStatus",
+        args: [],
+    });
+    return { status, refetchStatus, isStatusLoading, isError, error };
+};
